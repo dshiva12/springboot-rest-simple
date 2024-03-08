67d90f8f5f0b46f86352c67972bbb4961f652a79
Shivanand Shamrao deore <deore_shiva@rediffmail.com>
2024-03-09 08:40:50 +1100

    Initial commit

---
+++ src/main/java/com/shiva/restsimple/SpringbootRestSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.restsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiva/restsimple/SpringbootRestSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva.restsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
